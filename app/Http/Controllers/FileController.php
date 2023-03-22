e51c87b027e3fc529e4752241b41fd27bd189c8f
HTM <haythamaaraba7@gmail.com>
2023-03-22 20:45:10 +0000

    cleaned FileController

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -10,45 +10,26 @@ class FileController extends Controller
 {
     public function upload(Request $request)
     {
-        /*$this->validate($request, [
-            'file' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-        ]);*/
         $post=new Post;
         if($request->hasFile('image'))
         {
-            // $completeFileName=$request->file('image')->getClientOriginalName();
-            // $fileNameOnly=pathinfo($completeFileName,PATHINFO_FILENAME);
-            // $extension=$request->file('image')->getClientOriginalExtension();
-            // $comppic=str_replace(' ','_',$fileNameOnly.'-'.rand().'_'.time().'.'.$extension);
-            // $path = $request->file('image')->store('images', 'public'); 
             $post->image= $request->file('image')->store('images', 'public');
-            // $post->Path = $path;
             $post->text = $request->input('text');
             $post->title = $request->input('title');
             $post->link = $request->input('link');
-        } 
-    if($post->save())
-    {
-        return response()->json(['success'=>'Image Uploaded Successfully',"image"=>$post->image]);
-    }
-    else{
-        return response()->json(['error'=>'Image Upload Failed']);
-    }
-        //$file = $request->file('file');
-        //$file->store('public');
+        }
+        
+        if($post->save())
+        {
+            return response()->json(['success'=>'Image Uploaded Successfully',"image"=>$post->image]);
+        }
+        else{
+            return response()->json(['error'=>'Image Upload Failed']);
+        }
     }
     public function getImages()
     {
-        $storagePath = storage_path();
-
-        // Get the contents of the public directory
-        // $files = File::allFiles($publicPath);
         $posts = Post::all();
-
-        // Loop through each file and print its name
-        // foreach ($posts as $post) {
-        //     $post->image = base64_encode(file_get_contents( $storagePath . '\\app\\' . $post->Path));
-        // }
         return response()->json($posts);
     }
 }
